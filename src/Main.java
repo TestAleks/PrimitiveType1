7cae04bf17bfb8d2187eee0f44f3a4c5e66f2ad6
Alex <aleksei.erof@gmail.com>
2023-05-19 10:53:27 +0500

    bonus

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13676;
+        int bonuse = (amount / 20);
+        System.out.println(bonuse + " бонус");
+    }
+}
